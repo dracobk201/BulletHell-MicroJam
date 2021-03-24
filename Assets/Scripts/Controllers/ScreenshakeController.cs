be7e617f7326e0042eead836edb2290d72e07616
Erick Subero <suberoerick@gmail.com>
2021-03-23 23:37:48 -0400

    Screenshake
    - Screenshake implemented.

---
+++ Assets/Scripts/Controllers/ScreenshakeController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using ScriptableObjectArchitecture;
+
+public class ScreenshakeController : MonoBehaviour
+{
+    [SerializeField] private FloatReference lowShakeMagnitude = default(FloatReference);
+    [SerializeField] private FloatReference mediumShakeMagnitude = default(FloatReference);
+    [SerializeField] private FloatReference heavyShakeMagnitude = default(FloatReference);
+    [SerializeField] private FloatReference lowShakeDuration = default(FloatReference);
+    [SerializeField] private FloatReference mediumShakeDuration = default(FloatReference);
+    [SerializeField] private FloatReference heavyShakeDuration = default(FloatReference);
+    [SerializeField] private FloatReference shakeDampingSpeed = default(FloatReference);
+    private float _shakeDuration;
+    private float _shakeMagnitude;
+    private float _dampingSpeed;
+    private Vector3 _initialPosition;
+
+    private void OnEnable()
+    {
+        _initialPosition = transform.localPosition;
+        _dampingSpeed = shakeDampingSpeed.Value;
+    }
+
+    private void Update()
+    {
+        if (_shakeDuration > 0)
+        {
+            transform.localPosition = _initialPosition + Random.insideUnitSphere * _shakeMagnitude;
+            _shakeDuration -= Time.deltaTime * _dampingSpeed;
+        }
+        else
+        {
+            _shakeDuration = 0f;
+            transform.localPosition = _initialPosition;
+        }
+    }
+
+    public void TriggerSimpleShake()
+    {
+        _shakeDuration = lowShakeDuration.Value;
+        _shakeMagnitude = lowShakeMagnitude.Value;
+    }
+
+    public void TriggerMediumShake()
+    {
+        _shakeDuration = mediumShakeDuration.Value;
+        _shakeMagnitude = mediumShakeMagnitude.Value;
+    }
+
+    public void TriggerHardShake()
+    {
+        _shakeDuration = heavyShakeDuration.Value;
+        _shakeMagnitude = heavyShakeMagnitude.Value;
+    }
+}
