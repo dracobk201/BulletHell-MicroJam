7d9f3d820f4ece93964bf9ea75be97bfd16a69c8
Erick Subero <suberoerick@gmail.com>
2021-03-23 20:30:04 -0400

    Ui Fixes
    - Added Enemies Killed to UI.
    - All Anchors has been adjusted.

--- Assets/Scripts/Controllers/GameplayCanvasActions.cs
+++ Assets/Scripts/Controllers/GameplayCanvasActions.cs
@@ -1,15 +1,28 @@
 using UnityEngine;
 using ScriptableObjectArchitecture;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameplayCanvasActions : MonoBehaviour
 {
     [SerializeField] private IntReference playerActualHealth = default(IntReference);
     [SerializeField] private IntReference playerMaxHealth = default(IntReference);
+    [SerializeField] private IntReference enemiesKilled = default(IntReference);
     [SerializeField] private Image healthGauge = default(Image);
+    [SerializeField] private TextMeshProUGUI enemiesKilledLabel = default(TextMeshProUGUI);
+
+    private void Start()
+    {
+        enemiesKilledLabel.text = "0";
+    }
 
     public void UpdateHealthGauge()
     {
         healthGauge.fillAmount = (float) playerActualHealth.Value / (float) playerMaxHealth.Value;
+    }
+
+    public void UpdateEnemiesKilled()
+    {
+        enemiesKilledLabel.text = enemiesKilled.Value.ToString();
     }
 }
